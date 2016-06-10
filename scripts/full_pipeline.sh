ba238d8851cdc49fd6d193ab2e6391193bfccf7d
Caglar <ca9lar@gmail.com>
2016-06-10 14:51:47 -0400

    Fixed the validation issue.

--- scripts/full_pipeline.sh
+++ scripts/full_pipeline.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash -e
 
 
 NEWS_SOURCE="cnn"
@@ -10,17 +10,21 @@ VFILE=${DIR}/${VOCAB_NAME}
 
 preprocess_file() {
     MODE=$1
+    CLEAN=$2
+
     echo "$MODE"
-    echo "cleaning the ${MODE} file."
     ORIG_DIR=${DIR}/questions/${MODE}
     CLEAN_OUT_DIR=${DIR}/cleaned_${MODE}/
+    if [ ${CLEAN} -eq 1 ]
+    then
+        echo "cleaning the ${MODE} file."
+        mkdir -p ${CLEAN_OUT_DIR}
+        python clean_data.py --source_dir ${ORIG_DIR} --dest ${CLEAN_OUT_DIR}
+        echo "saved the cleaned ${MODE} file to ${CLEAN_OUT_DIR}."
+        echo
+    fi
 
-    mkdir -p ${CLEAN_OUT_DIR}
-    python clean_data.py --source_dir ${ORIG_DIR} --dest ${CLEAN_OUT_DIR}
-    echo "saved the cleaned ${MODE} file to ${CLEAN_OUT_DIR}."
-    echo
-
-    if [ "${MODE}" == "validation" ]
+    if [ "${MODE}" == "training" ]
     then
         echo "creating the vocab"
         python preprocess.py --source_file_dir ${CLEAN_OUT_DIR} --out_file_name ${VFILE} --max_vocab_size ${MAX_VOCAB_SIZE}
@@ -38,8 +42,8 @@ preprocess_file() {
     fi
 }
 
-preprocess_file training
-preprocess_file validation
-preprocess_file test
+preprocess_file training 1
+preprocess_file validation 1
+preprocess_file test 1
 
 exit;
